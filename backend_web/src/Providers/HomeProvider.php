792bed7536a9978368ecf80bd5b3244d16199e0c
ioedumac <eacevedof@yahoo.es>
2020-04-30 00:42:31 +0200

    servicios en home ok

--- backend_web/src/Providers/HomeProvider.php
+++ backend_web/src/Providers/HomeProvider.php
@@ -24,4 +24,26 @@ class HomeProvider
             ],
         ];
     }
+
+    public function get_text_servces_cards():array
+    {
+        return [
+            [
+                "background_image"=>"index.jpg",
+                "home_title"=>"<h1>cosmética <span>recogidos</span> PeRmAnEnTeS.</h1>",
+                //"home_link"=>"",
+            ],
+            [
+                "background_image"=>"slide-01.jpeg",
+                "home_title"=>"<h1>cejas <span>KeRaTiNa</span> manicura.</h1>",
+                //"home_link"=>"",
+            ],
+            [
+                "background_image"=>"slide-02.jpeg",
+                "home_title"=>"<h1>PeInAdO <span>complementos</span> estilo.</h1>",
+                //"home_link"=>"",
+            ],
+        ];
+    }
+
 }
