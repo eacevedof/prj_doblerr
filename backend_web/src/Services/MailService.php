d6c29f7690fed1ba8caa34586b5a165ee0f811ce
ioedumac <eacevedof@yahoo.es>
2020-04-30 10:34:37 +0200

    servici MailService

---
+++ backend_web/src/Services/MailService.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Services;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+
+class MailService
+{
+    private $mailer;
+    private $data;
+
+    public function __construct(MailerInterface $mailer,array $data)
+    {
+        $this->data = $data;
+        $this->mailer = $mailer;
+    }
+
+    private function get_mail_object()
+    {
+        $email = new Email();
+        $email->from($this->data["from"]);
+    }
+
+    public function send()
+    {
+        $email = (new Email())
+            ->from('hello@example.com')
+            ->to('you@example.com')
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject('Time for Symfony Mailer!')
+            ->text('Sending emails is fun again!')
+            ->html('<p>See Twig integration for better HTML integration!</p>');
+
+        $this->mailer->send($email);
+    }
+}
