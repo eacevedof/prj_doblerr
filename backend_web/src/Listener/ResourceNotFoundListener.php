bd057cc8ef95d3583c7663423ba782467c3036eb
ioedumac <eacevedof@yahoo.es>
2020-05-01 20:44:02 +0200

    el listener ahora vuelve a capturar 404

--- backend_web/src/Listener/ResourceNotFoundListener.php
+++ backend_web/src/Listener/ResourceNotFoundListener.php
@@ -6,6 +6,7 @@ namespace App\Listener;
 use Psr\Container\ContainerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 
 class ResourceNotFoundListener
 {
@@ -22,36 +23,33 @@ class ResourceNotFoundListener
         // You get the exception object from the received event
         $exception = $event->getThrowable();
 
-        //print_r(get_class($exception));
-        print_r(get_parent_class($exception));
-        //Symfony\Component\HttpKernel\Exception\NotFoundHttpException
-        if (!($exception instanceof Symfony\Component\HttpKernel\Exception\NotFoundHttpException || $exception instanceof Symfony\Component\Routing\Exception\ResourceNotFoundException))
-            return;
+        if($exception instanceof HttpExceptionInterface)
+        {
+            $message = sprintf(
+                'Resource: %s with code: %s',
+                $exception->getMessage(),
+                $exception->getCode()
+            );
 
-        $message = sprintf(
-            'Resource: %s with code: %s',
-            $exception->getMessage(),
-            $exception->getCode()
-        );
+            $engine = $this->container->get('twig');
+            $content = $engine->render("errors/404.html.twig", ["message" => $message]);
 
-        $engine = $this->container->get('twig');
-        $content = $engine->render("errors/404.html.twig",["message"=>$message]);
+            // Customize your response object to display the exception details
+            $response = new Response();
+            $response->setContent($content);
 
-        // Customize your response object to display the exception details
-        $response = new Response();
-        $response->setContent($content);
+            // HttpExceptionInterface is a special type of exception that
+            // holds status code and header details
+            if ($exception instanceof ResourceNotFoundException) {
+                $response->setStatusCode($exception->getStatusCode());
+                $response->headers->replace($exception->getHeaders());
+            } else {
+                $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
+            }
 
-        // HttpExceptionInterface is a special type of exception that
-        // holds status code and header details
-        if ($exception instanceof ResourceNotFoundException) {
-            $response->setStatusCode($exception->getStatusCode());
-            $response->headers->replace($exception->getHeaders());
-        } else {
-            $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
+            // sends the modified response object to the event
+            $event->setResponse($response);
         }
-
-        // sends the modified response object to the event
-        $event->setResponse($response);
     }
 
 
