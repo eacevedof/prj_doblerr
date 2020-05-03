60e2fb70b75d10f7b0799c7338a28949b5962ae5
ioedumac <eacevedof@yahoo.es>
2020-05-03 16:11:57 +0200

    tocando emailservice

--- backend_web/src/Services/EmailService.php
+++ backend_web/src/Services/EmailService.php
@@ -19,14 +19,14 @@ final class EmailService extends BaseService
     {
         $action = "cita";
         $name = $this->get_post("name") ?? "aaa";
-        $emailto = $this->get_post("mail-to") ?? "eacevedofgmail.com";
+        $emailto = $this->get_post("mail-to") ?? "eacevedof@gmail.com";
 
         $message = $this->get_post("message") ?? " message ";
         $data = [
             "from" => $this->get_env("APP_EMAIL_FROM"),
             "to" => $emailto,
             "bcc" => [$this->get_env("APP_EMAIL_FROM"), $this->get_env("APP_EMAIL_TO")],
-            "subject" => sprintf("doblerr.es - %s de (%s) %s %s",$action,$name,$emailto,date("Ymd-His")),
+            "subject" => sprintf("doblerr.es - %s de %s (%s) %s",$action,$name,$emailto,date("Ymd-His")),
             "text" => $message,
         ];
         $mail = new Mail($data,$this->mailer);
