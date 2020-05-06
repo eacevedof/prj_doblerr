c5fc0e39ea5557d94143f1b9211b28382426ffe4
ioedumac <eacevedof@yahoo.es>
2020-05-06 21:32:08 +0200

    uso de invoke

--- backend_web/src/Controller/Common/SecurityController.php
+++ backend_web/src/Controller/Common/SecurityController.php
@@ -2,37 +2,14 @@
 //proyecto\src\Controller\Common\SecurityController.php
 namespace App\Controller\Common;
 
-use App\Controller\BaseController;
-use App\Repository\UserRepository;
-use Symfony\Component\HttpFoundation\Request;
-
-use App\Entity\User;
-use App\Form\RegisterType;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use App\Controller\BaseController;
 
 class SecurityController extends BaseController
 {
-
-    /**
-     * @var UserRepository
-     */
-    private UserRepository $userRepository;
-
-    public function __construct(UserRepository $userRepository)
-    {
-        $this->userRepository = $userRepository;
-    }
-
-    public function login(AuthenticationUtils $authentication)
+    public function __invoke(AuthenticationUtils $authentication)
     {
         $error = $authentication->getLastAuthenticationError();
-
-        if($error) {
-            print_r("SecurityController.login");
-            dump($error);
-            die;
-        }
         $lastUsername = $authentication->getLastUsername();
         return $this->render("open/security/login.html.twig",[
             "error" => $error,
