c1587f81f84370b6dc9a7b8571e9ca0dac46aada
ioedumac <eacevedof@yahoo.es>
2020-05-07 12:49:13 +0200

    navbar ok

--- frontend_react/pannel/src/App.js
+++ frontend_react/pannel/src/App.js
@@ -1,17 +1,21 @@
 //App.js
 import React, {useState, useEffect} from 'react';
 import Sidebar from "./components/common/sidebar"
+import Navbar from "./components/common/navbar"
 
 //import axios from 'axios'
 console.log(Sidebar)
 
 function App() {
   return (
-    <div>
+    <div className="wrapper">
       {/* 
       <img src={process.env.PUBLIC_URL + '/assets/img/sidebar-5.jpg'} />  
       */}
-      <Sidebar/>
+      <Sidebar />
+      <div className="main-panel">
+        <Navbar />
+      </div>
     </div>
   )
 }//App()
