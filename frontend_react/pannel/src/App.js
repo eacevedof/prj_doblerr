850d24442f395bf7a1510d41bb51bef890d188c7
ioedumac <eacevedof@yahoo.es>
2020-05-10 11:53:08 +0200

    front: app.js recupero pedido de localstorage

--- frontend_react/pannel/src/App.js
+++ frontend_react/pannel/src/App.js
@@ -11,14 +11,24 @@ import {
 import ProductList from './components/product/product_list';
 import OrderDetail from "./components/order/order_detail"
 import objorder from "./models/order"
-
+import LocalDb from "./helpers/local_db"
 
 function App(){
+
+
 
   const [order, set_order] = useState(objorder)
   console.log("App.order ",order)
 
-
+  useEffect(() => {
+    console.log("app.useffect")
+    if(order.products.length === 0){
+      const dborder = LocalDb.select("order")
+      if(dborder.products.length>0)
+        console.log("app.useeffect.dborder",dborder)
+        set_order(dborder)
+    }
+  }, []);
 
   return (
     <Router>
