4d39df3a45597d69ec78e49c2d6a23d590c411b6
ioedumac <eacevedof@yahoo.es>
2020-06-26 19:51:53 +0200

    backend: promo subscribe

--- backend_web/src/Controller/Open/PromotionController.php
+++ backend_web/src/Controller/Open/PromotionController.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 namespace App\Controller\Open;
 use App\Component\Serialize;
 use App\Providers\SeoProvider;
+use App\Services\EmailService;
 use Symfony\Component\HttpFoundation\Request;
 use App\Controller\BaseController;
 use App\Services\Common\ProductService;
+use Symfony\Component\HttpFoundation\Response;
 
 class PromotionController extends BaseController
 {
@@ -29,6 +31,36 @@ class PromotionController extends BaseController
                 ["value"=>"","text"=>""],
             ],
         ]);
+    }
+
+    public function subscribe(Request $request)
+    {
+        $this->logd($_POST,"mail.post");
+        $this->logd($_SERVER["REMOTE_ADDR"],"ip from");
+        try{
+            $mail = new EmailService($this->get_request(),$mailer);
+            $mail->send();
+        }
+        catch(\Exception $e){
+            $this->logd($e->getMessage(),"mail.error");
+            return (new Response('Content',
+                Response::HTTP_BAD_REQUEST,
+                ['content-type' => 'application/json']))->setContent(json_encode(
+                [
+                    "title" => "error",
+                    "description"=>$e->getMessage()
+                ]
+            ));
+        }
+
+        return (new Response('Content',
+            Response::HTTP_OK,
+            ['content-type' => 'application/json']))->setContent(json_encode(
+            [
+                "title" => "success",
+                "description"=>"Email has benn sent"
+            ]
+        ));
     }
 
 }//PromotionController
