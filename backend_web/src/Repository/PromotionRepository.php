2f9080753825caeffae852aee7bac022883b3d34
ioedumac <eacevedof@yahoo.es>
2020-06-28 13:49:09 +0200

    corrijo query fechas

--- backend_web/src/Repository/PromotionRepository.php
+++ backend_web/src/Repository/PromotionRepository.php
@@ -53,9 +53,10 @@ class PromotionRepository extends BaseRepository
             ->andWhere("p.slug=:slug")->setParameter("slug",$slug)
             //->andWhere("p.dateFrom>=:today")->setParameter("today",$today)
             //->andWhere("p.dateTo<=:today")->setParameter("today",$today);
-            ->andWhere("p.dateFrom>=:today")->setParameter("today",new \DateTime(), \Doctrine\DBAL\Types\Types::DATE_MUTABLE)
-            ->andWhere("p.dateTo<=:today")->setParameter("today",new \DateTime(), \Doctrine\DBAL\Types\Types::DATE_MUTABLE);
-
+            //->andWhere("p.dateFrom>=:today")->setParameter("today",new \DateTime(), \Doctrine\DBAL\Types\Types::DATE_MUTABLE)
+            //->andWhere("p.dateTo<=:today")->setParameter("today",new \DateTime(), \Doctrine\DBAL\Types\Types::DATE_MUTABLE);
+            ->andWhere("p.dateFrom>='$today'")
+            ->andWhere("p.dateTo>'$today'");
 
         $q = $qb->getQuery();
         $this->log($q->getDQL(),"promorepo.findByDate.q");
