734d4816c79bc207ea00aec8c4597942c7bbe5db
ioedumac <eacevedof@yahoo.es>
2020-06-29 17:27:06 +0200

    cierre clase: PromotionSubscriptionService

--- backend_web/src/Services/Open/PromotionSubscriptionService.php
+++ backend_web/src/Services/Open/PromotionSubscriptionService.php
@@ -184,4 +184,4 @@ class PromotionSubscriptionService extends BaseService
         $promosubscription->setCode1($finalcode);
         $this->promotionsSubscribersRepository->save($promosubscription);
     }
-}
+}// PromotionSubscriptionService
