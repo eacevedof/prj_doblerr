70cc3d3c0bad3d20d135d57e48e0b6562b6e0a7c
ioedumac <eacevedof@yahoo.es>
2020-07-01 15:32:27 +0200

    AppPromotionsSubscriptions.php sincronizo cambios bd

--- backend_web/src/Entity/App/AppPromotionsSubscriptions.php
+++ backend_web/src/Entity/App/AppPromotionsSubscriptions.php
@@ -7,7 +7,6 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * AppPromotionsSubscriptions
- *
  * @ORM\Table(name="app_promotions_subscriptions")
  * @ORM\Entity
  */
@@ -15,7 +14,6 @@ class AppPromotionsSubscriptions extends BaseEntity
 {
     /**
      * @var int
-     *
      * @ORM\Column(name="id", type="integer", nullable=false)
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="IDENTITY")
@@ -24,56 +22,54 @@ class AppPromotionsSubscriptions extends BaseEntity
 
     /**
      * @var int
-     *
      * @ORM\Column(name="id_promotion", type="integer", nullable=false)
      */
     private $idPromotion;
 
     /**
      * @var int
-     *
      * @ORM\Column(name="id_promouser", type="integer", nullable=false)
      */
     private $idPromouser;
 
     /**
      * @var \DateTime|null
-     *
      * @ORM\Column(name="date_subs", type="datetime", nullable=true, options={"default"="NULL"})
      */
     private $dateSubs = null;
 
     /**
      * @var string|null
-     *
+     * @ORM\Column(name="url_ref", type="string", length=250, nullable=true, options={"default"="NULL"})
+     */
+    private $urlRef = null;
+
+    /**
+     * @var string|null
      * @ORM\Column(name="code1", type="string", length=15, nullable=true, options={"default"="NULL"})
      */
     private $code1 = null;
 
     /**
      * @var \DateTime|null
-     *
      * @ORM\Column(name="date_confirm", type="datetime", nullable=true, options={"default"="NULL"})
      */
     private $dateConfirm = null;
 
     /**
      * @var bool|null
-     *
      * @ORM\Column(name="is_confirmed", type="boolean", nullable=true)
      */
     private $isConfirmed = 0;
 
     /**
      * @var \DateTime|null
-     *
      * @ORM\Column(name="date_exec", type="datetime", nullable=true, options={"default"="NULL"})
      */
     private $dateExec = null;
 
     /**
      * @var string|null
-     *
      * @ORM\Column(name="notes", type="string", length=300, nullable=true, options={"default"="NULL"})
      */
     private $notes = null;
@@ -142,6 +138,22 @@ class AppPromotionsSubscriptions extends BaseEntity
     {
         $this->dateSubs = $dateSubs;
         return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getUrlRef(): ?string
+    {
+        return $this->urlRef;
+    }
+
+    /**
+     * @param string|null $urlRef
+     */
+    public function setUrlRef(?string $urlRef): void
+    {
+        $this->urlRef = $urlRef;
     }
 
     /**
@@ -233,7 +245,4 @@ class AppPromotionsSubscriptions extends BaseEntity
         $this->notes = $notes;
         return $this;
     }
-
-
-
 }
